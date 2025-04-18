93983122e92f3d61b2563f4be31175b2a6dd2206
0xNick404 <ngrimestt@gmail.com>
2025-04-17 22:33:24 -0400

    Update user.py

--- App/models/user.py
+++ App/models/user.py
@@ -3,22 +3,21 @@ from App.database import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username =  db.Column(db.String(20), nullable=False, unique=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
-    role = db.Column(db.String(20), nullable=False)
-    profile = db.relationship('UserProfile', backref='user', lazy=True)
+    type = db.Column(db.String(50))
+    __mapper_args__ = {
+        'polymorphic_identity': 'user',
+        'polymorphic_on': type
+    }
 
-    def __init__(self, username, password, role):
+    def __init__(self, username, name, email, password):
         self.username = username
+        self.name = name
+        self.email = email
         self.set_password(password)
-        self.role = role
-
-    def get_json(self):
-        return{
-            'id': self.id,
-            'username': self.username,
-            'role': self.role
-        }
 
     def set_password(self, password):
         """Create hashed password."""
@@ -28,75 +27,132 @@ class User(db.Model):
         """Check hashed password."""
         return check_password_hash(self.password, password)
     
-class UserProfile(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(50), nullable=False)
-    last_name = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    phone = db.Column(db.String(20))
-    university = db.Column(db.String(100))
-    major = db.Column(db.String(100))
-    gpa = db.Column(db.Float)
-    company = db.Column(db.String(100))
-    industry = db.Column(db.String(100))
-
-    def __init__(self, first_name, last_name, email, phone=None, university, major, gpa, company, industry):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.phone = phone
-        self.address = address
-        self.university = university
-        self.major = major
-        self.gpa = gpa
-        self.company = company
-        self.industry = industry 
+    def __repr__(self):
+        return f'<User {self.id} {self.username} - {self.email}>'
 
     def get_json(self):
         return {
-            'id': self.id,
-            'first_name': self.first_name,
-            'last_name': self.last_name,
-            'email': self.email,
-            'phone': self.phone,
-            'university': self.university,
-            'major': self.major,
-            'gpa': self.gpa,
-            'company': self.company,
-            'industry': self.industry
-        }                                                                                   
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "type": self.type
+        }
+    
+class Student(User):
+  __tablename__ = 'student'
+  __mapper_args__ = {
+      'polymorphic_identity': 'student',
+  }
 
-class Company(db.Model):
-    __tablename__ = 'company'
+  def __init__(self, username, name, email, password):
+    super().__init__(username, name, email, password)
+
+  def apply_internship(self, internship, file="dummyfilestring"):
+    new_application = Application(internship, self, file)
+    self.applications.append(new_application)
+    return new_application
+
+class Company(User):
+  __tablename__ = 'company'
+  __mapper_args__ = {
+      'polymorphic_identity': 'company',
+  }
+
+  def __init__(self, username, name, email, password):
+    super().__init__(username, name, email, password)
+
+  def create_internship(self, title, description, salary):
+    newinternship = Internship(title, description, salary)
+    self.internships.append(newinternship)
+    return newinternship
+
+  def update_app(self, app, status):
+    if app.internship.company == self:
+      app.status = status
+      db.session.commit()
+    
+class Staff(User):
+  __tablename__ = 'staff'
+  __mapper_args__ = {
+      'polymorphic_identity': 'staff',
+  }
+
+  def __init__(self, username, name, email, password):
+    super().__init__(username, name, email, password)
+    
+  def create_shortlist(self, student, internship):
+    shortlist_entry = Shortlist(student=student, internship=internship)
+    db.session.add(shortlist_entry)
+    db.session.commit()
+    return shortlist_entry
+    
+class Internship(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(80), nullable=False)
+  description = db.Column(db.String(120), nullable=False)
+  start_date = db.Column(db.String(20), nullable=False)
+  end_date = db.Column(db.String(20), nullable=False)
+  salary = db.Column(db.Integer, nullable=False)
+  company_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+  company = db.relationship('Company', backref=db.backref('internships', lazy=True))
+
+  def __init__(self, title, description, start_date, end_date, salary):
+    self.title = title
+    self.description = description
+    self.start_date = start_date
+    self.end_date = end_date
+    self.salary = salary
+    
+  def get_json(self):
+    return {
+        "id": self.id,
+        "title": self.title,
+        "start date": self.start_date,
+        "end date": self.end_date,
+        "description": self.description,
+        "salary": self.salary,
+    }
+
+class Application(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  internship_id = db.Column(db.Integer, db.ForeignKey('internship.id'), nullable=False)
+  student_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+  status = db.Column(db.String(80), nullable=False, default='pending')
+  resume_url = db.Column(db.String(120), nullable=False)
+  student = db.relationship('Student', backref=db.backref('applications', lazy=True))
+  internship = db.relationship('Internship', backref=db.backref('applications', lazy=True))
+
+  def __init__(self, internship, student, url="https://file.pdf"):
+    self.internship = internship
+    self.student = student
+    self.resume_url =  url
+
+  def get_json(self):
+    return {
+        "id": self.id,
+        "internship_id": self.internship_id,
+        "internship": self.internship.title,
+        "student_id": self.student_id,
+        "student": self.student.username,
+        "status": self.status,
+        "resume_url": self.resume_url
+    }
+    
+class Shortlist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    company_name = db.Column(db.String(100), nullable=False)
-    industry = db.Column(db.String(100), nullable=False)
-
-    def __init__(self, company_name, industry):
-        self.company_name = company_name
-        self.industry = industry
-
+    internship_id = db.Column(db.Integer, db.ForeignKey('internship.id'), nullable=False)
+    student_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    staff_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    student = db.relationship('Student', backref=db.backref('shortlists', lazy=True))
+    internship = db.relationship('Internship', backref=db.backref('shortlisted_students', lazy=True))
+    
     def get_json(self):
         return {
-            'id': self.id,
-            'company_name': self.company_name,
-            'industry': self.industry
+            "id": self.id,
+            "internship_id": self.internship_id,
+            "internship": self.internship.title,
+            "student_id": self.student_id,
+            "student": self.student.username,
         }
 
-class Internship(Company):
-    __tablename__ = 'internship'
-    id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    position = db.Column(db.String(100), nullable=False)
-    start_date = db.Column(db.String(120), nullable=False)
-    end_date = db.Column(db.String(120), nullable=False)
-    description = db.Column(db.String(120), nullable=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    def __init__(self, position, start_date, end_date, description=None):
-        super().__init__(company_name=None, industry=None)
-        self.position = position
-        self.start_date = start_date
-        self.end_date = end_date
-        self.description = description
-    
 
